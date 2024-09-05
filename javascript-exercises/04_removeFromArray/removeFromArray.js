458b3f53da9eb9b28ebcfc97ad5efaacbfdb8c15
sang53 <143582855+sang53@users.noreply.github.com>
2024-09-05 17:06:15 +1000

    Complete javascript exercise 04

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
 const removeFromArray = function() {
+    let output = [];
+    let hs = new Set();
+    for (let i = 1; i < arguments.length; i++) {
+        hs.add(arguments[i]);
+    }
+    for (let i = 0; i < arguments[0].length; i++) {
+        if (!hs.has(arguments[0][i])) {
+            output.push(arguments[0][i]);
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
