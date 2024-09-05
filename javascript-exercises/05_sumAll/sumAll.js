dbcefb73267fc71cbbd03dd121b1f4a0fab8c086
sang53 <143582855+sang53@users.noreply.github.com>
2024-09-05 17:30:57 +1000

    Complete javascript exercise 05

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+    if (start < 0 || end < 0) return 'ERROR';
+    let sum = 0;
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+    for (;start <= end; start++) {
+        sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
