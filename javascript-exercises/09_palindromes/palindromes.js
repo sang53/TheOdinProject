b93f97bb760b8c2616f4bedd66d3bfab2ecfd7ef
sang53 <143582855+sang53@users.noreply.github.com>
2024-09-07 15:01:01 +1000

    Complete javascript exercise 09

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let temp = input.replace(/\W/g, "").toLowerCase();
+    let left = 0;
+    let right = temp.length - 1;
+    
+    while (left < right) {
+        if (temp[left] != temp[right]) return false;
+        left += 1;
+        right -= 1;
+    }
+    return true;
+    
 };
 
 // Do not edit below this line
