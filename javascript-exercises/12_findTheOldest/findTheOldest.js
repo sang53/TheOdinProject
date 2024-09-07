80996a3cb1047efb91fa0452e9a78d03f7c68f7f
sang53 <143582855+sang53@users.noreply.github.com>
2024-09-07 18:37:28 +1000

    Complete javascript exercise 12

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const CURRENT_YEAR = new Date().getFullYear();
+    people.forEach(element => {
+        if (!element.yearOfDeath) element.yearOfDeath = CURRENT_YEAR;
+    });
+    return people.reduce((oldPerson, person) => 
+        oldPerson.yearOfDeath - oldPerson.yearOfBirth < person.yearOfDeath - person.yearOfBirth ? person : oldPerson
+    );
 };
 
 // Do not edit below this line
