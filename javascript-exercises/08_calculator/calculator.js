d61b96ddbcfa5413a0b3ee8051e4a01a11c78ffa
sang53 <143582855+sang53@users.noreply.github.com>
2024-09-07 14:37:45 +1000

    Complete javascript exercise 08

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((total, current) => total + current, 0);
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1];
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, raisePower) {
+	return Math.pow(base, raisePower);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  for (i = num - 1; i > 0; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
