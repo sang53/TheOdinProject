2844650fd21f65cb7a375942ad4ae57a3d9b3d50
sang53 <143582855+sang53@users.noreply.github.com>
2024-09-07 16:23:00 +1000

    Complete javascript exercise 10

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    if (num <= 2) return 1;
+    let prev = 1;
+    let curr = 1;
 
+    for (i = 3; i <= num; i++) {
+        [curr, prev] = [prev + curr, curr];
+    }
+    return curr;
 };
 
 // Do not edit below this line
