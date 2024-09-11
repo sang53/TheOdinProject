9e87f9c92392a64600ccab1a3270a614beb10244
sang53 <143582855+sang53@users.noreply.github.com>
2024-09-11 19:14:39 +1000

    Complete Scissors Paper Rock GUI script
    Add button shadow on hover

--- scissors-paper-rock/script.js
+++ scissors-paper-rock/script.js
@@ -1,57 +1,65 @@
 const NUM_ROUNDS = 5;
-const COMPARE = ["scissors", "rock", "paper", "scissors"]
-let playerScore = 0;
-let computerScore = 0;
+const convertToStr = ["Scissors", "Paper", "Rock", "Scissors"];
+const scores = document.querySelectorAll(".score");
+const textOutput = document.querySelector("#round");
+const playButton = document.querySelector("#play");
+const divButtons = document.querySelector("#buttons");
 
-function playRound() {
-    let input = getUserInput();
+var playerScore = 0;
+var computerScore = 0;
+var gameStarted = false;
+
+playButton.addEventListener("click", playGame);
+divButtons.addEventListener("click", playRound);
+
+function playRound(e) {
+    if (!gameStarted || e.target === divButtons) return;
     let response = Math.floor((Math.random() * 3)) + 1;
-    generateWinner(input, response);
-}
-
-function getUserInput() {
-    let input;
-    do {
-        input = prompt("Please enter Scissors, Paper or Rock")
-        input = input.toLowerCase();
-        if (input === "scissor" || input === "scissors") {
-            return "scissors"
-        }
-    } while (input != "rock" && input != "paper");
-    return input;
+    generateWinner(e.target.id, response);
 }
 
 function generateWinner(input, response) {
-    let output = "The computer played " + COMPARE[response] + ".\n";
-    if (input === COMPARE[response]) {
-        output = output + "It's a draw!\n";
-    }
-    else if (input === COMPARE[response - 1]) {
-        output = output + "You Lose!\n";
-        computerScore++;
-    }
-    else {
-        output = output + "You Win!\n";
+    let outputString = "You Played: " + convertToStr[input] + "\nThe Computer Played: " + convertToStr[response] + "\n";
+    if (convertToStr[input] === convertToStr[response]) outputString += "It's a Draw!";
+    else if (input == response - 1) {
+        outputString += "You Win!";
         playerScore++;
-    }
-    alert(output.concat("Scores:\nPlayer - ", playerScore, "\nComputer - ", computerScore))
-    return;
-}
-function playGame() {
-    alert("Play 5 rounds of scissors, paper, rock!")
-    for(i = 0; i < NUM_ROUNDS; i++) {
-        playRound();
-    }
-    if (playerScore > computerScore) {
-        alert("You win the game!");
-    }
-    else if (playerScore < computerScore) {
-        alert("You lose the game...");
+        updateScores();
     }
     else {
-        alert("The game ends in a draw.");
+        outputString += "You Lose!";
+        computerScore++;
+        updateScores();
     }
-    alert("Thanks for playing!");
+    textOutput.innerText = outputString;
 }
 
-playGame();
+function updateScores() {
+    scores[0].textContent = playerScore;
+    scores[1].textContent = computerScore;
+    if (playerScore === 5) {
+        scores[0].textContent += " Win!";
+        endGame();
+    }
+    else if (computerScore === 5) {
+        scores[1].textContent += " Win!";
+        endGame();
+    }
+}
+
+function resetScore(element) {
+    element.textContent = 0;
+}
+
+function playGame() {
+    playerScore = 0;
+    computerScore = 0;
+    scores.forEach(resetScore);
+    playButton.textContent = "Reset Game";
+    gameStarted = true;
+}
+
+function endGame() {
+    gameStarted = false;
+    playButton.textContent = "Play Again!"
+}
