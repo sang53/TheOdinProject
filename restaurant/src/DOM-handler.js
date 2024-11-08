2b4de0612a94acf46016ed9eed53d17bebd4df29
sang53 <143582855+sang53@users.noreply.github.com>
2024-11-08 12:31:00 +1100

    Finish Restaurant Project
    Added additional js file

---
+++ restaurant/src/DOM-handler.js
@@ -0,0 +1,6 @@
+export function createElement(tag="div", classes=[], text="") {
+    const element = document.createElement(tag);
+    element.className = classes.join(" ");
+    element.innerText = text;
+    return element;
+}   
--- restaurant/src/index.js
+++ restaurant/src/index.js
@@ -1,1 +1,40 @@
 import "./style.css";
+import { addMenu } from "./menu.js";
+import { addHome } from "./home.js";
+import { addContacts } from "./contact.js";
+
+const CONTENTDIV = document.querySelector("#content");
+CONTENTDIV.className = "flex column";
+
+document.addEventListener("click", eventHandler)
+
+let currentPage = "home";
+addHome();
+
+function eventHandler(event) {
+    if (event.target.id !== currentPage) {
+        switch (event.target.id) {
+            case "home":
+                removeNodes();
+                addHome();
+                currentPage = "home";
+                break;
+            case "menu":
+                removeNodes();
+                addMenu();
+                currentPage = "menu";
+                break;
+            case "contact":
+                removeNodes();
+                addContacts();
+                currentPage = "contact";
+                break;
+        }
+    }
+}
+
+function removeNodes() {
+    while (CONTENTDIV.firstElementChild) {
+        CONTENTDIV.firstElementChild.remove();
+    }
+}
