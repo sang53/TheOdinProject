5c2f97cd5ec001880627d5f72a5dafd0afacb651
sang53 <143582855+sang53@users.noreply.github.com>
2024-11-27 04:42:10 +1100

    Complete GIF search Project

---
+++ gif-search/src/index.js
@@ -0,0 +1,38 @@
+import "./style.css";
+
+const BUTTON = document.querySelector("button");
+const SEARCHBAR = document.querySelector("input");
+
+BUTTON.addEventListener("click", () => {
+  if (BUTTON.disabled) return;
+  BUTTON.disabled = true;
+  GIFHANDLER.search(SEARCHBAR.value).finally(() => (BUTTON.disabled = false));
+});
+
+const GIFHANDLER = (function () {
+  const IMG = document.querySelector("img");
+
+  function search(searchterm = "nba") {
+    return fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=jRsC1wVvU18GW0p9CwzxWjdRszkBURCP&s=${searchterm}`,
+      { mode: "cors" },
+    )
+      .then(validateResponse)
+      .then(updateSRC)
+      .catch((error) => alert(error));
+  }
+
+  function validateResponse(response) {
+    if (!response.ok) return Promise.reject("Status Code: " + response.status);
+    else return response.json();
+  }
+
+  function updateSRC(responseJSON) {
+    if (!responseJSON.data) return Promise.reject("No GIFs found");
+    else IMG.src = responseJSON.data.images.original.url;
+  }
+
+  return { search };
+})();
+
+GIFHANDLER.search();
