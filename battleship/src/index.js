2a65cafb1971bdf44be6d4dfa2f03bb5bc159e44
sang53 <143582855+sang53@users.noreply.github.com>
2024-12-17 10:54:58 +1100

    add(battleship): start screen

--- battleship/src/index.js
+++ battleship/src/index.js
@@ -1,199 +1,88 @@
 import "./style.css";
-
-import { addToMain, openRules, openCustomise, switchScreen } from "./DOMOutput";
-import { makeBoard, makeControllers } from "./init";
-import { resetShipSelect, setupShipSelect } from "./shipSelect";
+import { makeElement, toggleClass } from "./DOM";
+import { Board } from "./Board";
+import { changeSettings, settings } from "./gameSettings";
+import {
+  getPlayButton,
+  getRulesButton,
+  getSettingsButton,
+  updateSettings,
+  resetDOM,
+  addToMain,
+  removeListeners,
+} from "./index-helpers";
 import { Player } from "./Player";
-import { cpuShips, getCpuShots } from "./cpu";
-import { battleScreen, processShots, setupShotSelect } from "./battle";
 
-const SIDES = 10;
-const SHIPS = 5;
-let shots;
+const RULES = document.querySelector("#rules-modal");
+const SETTINGS = document.querySelector("#settings-modal");
 
-let gameStage = 0;
-const steps = [
-  [
-    "rules",
-    "customise",
-    "ship select",
-    "reset ship select",
-    "switch",
-    "cpu ships",
-  ],
-  ["setup", "switch", "select shots", "process shots", "cpu shots"],
-  ["end screen", "stats", "play again"],
-];
-let stepIdx = 0;
-let currentTurn = 0;
+startScreen();
 
-let players = [];
-let controllers;
-
-initialise();
-
-// move to ./init???
-function initialise() {
-  // create players and their boards
-  for (let i = 0; i < 2; i++) {
-    const player = new Player(i, SHIPS);
-    player.setBoard(makeBoard(i, SIDES));
-    players.push(player);
-  }
-
-  // make & reference controllers
-  controllers = makeControllers();
-
-  // add boards (hidden) & controllers to DOM
-  addToMain(players[0].board.boardRef);
-  addToMain(controllers.container);
-  addToMain(players[1].board.boardRef);
-
-  // controller button = main method to progresses game
-  controllers.button.addEventListener("click", gameController);
+function startScreen() {
+  toggleClass(document.querySelector("#main"), "start-screen");
+  addToMain(makeElement("h1", [], "Play BattleShip!"));
+  addToMain(getBoardsDiv());
+  addToMain(getFooter());
+  updateSettings();
 }
 
-export function gameController() {
-  if (gameStage === 0) {
-    switch (steps[gameStage][stepIdx]) {
-      case "rules":
-        openRules(true);
-        break;
-      case "customise":
-        openCustomise();
-        break;
-      case "ship select":
-        shipSelect();
-        break;
-      case "reset ship select":
-        resetShipSelect();
-        break;
-      case "switch":
-        switchScreen(currentTurn);
-        break;
-      case "cpu ships":
-        cpuShips(players[1].board, SHIPS, SIDES);
-        break;
-    }
+function getBoardsDiv() {
+  const contentDiv = makeElement("div", [["id", "boards"]]);
 
-    switch (stepIdx) {
-      case 3:
-        currentTurn = toggleTurn();
-        // case: cpu => set cpu ships => next stage
-        if (!players[1].control) stepIdx = 5;
-        // case: second ship selection => next stage
-        else if (currentTurn === 0) nextStage();
-        // case: player0 ship selection => switch screen
-        else stepIdx++;
-        gameController();
-        break;
-      case 4:
-        stepIdx = 2;
-        break;
-      case 5:
-        currentTurn = toggleTurn();
-        nextStage();
-        gameController();
-        break;
-      default:
-        stepIdx++;
-        break;
-    }
-  } else if (gameStage === 1) {
-    switch (steps[gameStage][stepIdx]) {
-      case "setup":
-        battleScreen(players);
-        break;
-      case "switch":
-        switchScreen(currentTurn);
-        break;
-      case "select shots":
-        shotSelect();
-        break;
-      case "process shots":
-        processShots(players[toggleTurn()]);
-        break;
-      case "cpu shots":
-        cpuShots();
-        break;
-    }
+  contentDiv.appendChild(new Board(10, 5).boardRef);
+  contentDiv.appendChild(getPlayButton(nextStage));
+  contentDiv.appendChild(new Board(10, 5).boardRef);
 
-    switch (stepIdx) {
-      case 0:
-        stepIdx++;
-        gameController();
-        break;
-      case 3:
-        currentTurn = toggleTurn();
-        if (players[currentTurn].control) stepIdx = 1;
-        else stepIdx = 4;
-        break;
-      case 4:
-        stepIdx = 1;
-        gameController();
-        break;
-      default:
-        stepIdx++;
-        break;
-    }
-  } else if (gameStage === 2) {
-    switch (steps[gameStage][stepIdx]) {
-      case "end screen":
-        console.log("end", currentTurn);
-        break;
-    }
-  }
+  return contentDiv;
 }
 
-export function setCustomRules(inputObj) {
-  players[1].control = inputObj.player;
+function getFooter() {
+  const footerDiv = makeElement("div", [["id", "footer"]]);
 
-  if (inputObj.single) shots = "single";
-  else shots = "cluster";
-}
+  footerDiv.appendChild(getRulesButton(openRules));
+  footerDiv.appendChild(makeElement("div", [["id", "settings"]]));
+  footerDiv.appendChild(getSettingsButton(openSettings));
 
-function shipSelect() {
-  updateController("Place Your Ships!", "Done");
-  toggleControlButton(false);
-  setupShipSelect(players[currentTurn].board, SHIPS);
-}
-
-function shotSelect() {
-  const numShots = getNumShots(players[currentTurn]);
-  updateController(`Select ${numShots} Shots: `, "Fire!");
-  toggleControlButton(false);
-  setupShotSelect(
-    players[toggleTurn()].board,
-    numShots,
-    players[currentTurn].board,
-  );
-}
-
-function cpuShots() {
-  const numShots = getNumShots(players[currentTurn]);
-  const shots = getCpuShots(players[toggleTurn()], numShots);
-  processShots(shots);
-}
-
-export function updateController(instructionMsg, buttonMsg = "") {
-  controllers.instructions.innerText = instructionMsg;
-  if (buttonMsg) controllers.button.innerText = buttonMsg;
-}
-
-export function toggleControlButton(bool) {
-  controllers.button.disabled = !bool;
-}
-
-function toggleTurn() {
-  return currentTurn ? 0 : 1;
+  return footerDiv;
 }
 
 function nextStage() {
-  gameStage++;
-  stepIdx = 0;
+  removeListeners(openRules, openSettings);
+  resetDOM();
+  const players = [
+    new Player(0, true),
+    new Player(0, settings.opp === "Player"),
+  ];
+  shipPlace(players);
 }
 
-function getNumShots(playerObj) {
-  if (shots === "single") return 1;
-  else return playerObj.aliveShips;
+function openRules() {
+  RULES.showModal();
+  RULES.querySelector("button").addEventListener("click", closeRules, {
+    once: true,
+  });
+}
+
+function closeRules() {
+  RULES.close();
+}
+
+function openSettings() {
+  SETTINGS.showModal();
+  SETTINGS.querySelector("button").addEventListener("click", closeSettings, {
+    once: true,
+  });
+}
+
+function closeSettings(event) {
+  const formElements = SETTINGS.querySelector("form").elements;
+
+  changeSettings({
+    shot: formElements["cluster"].checked ? "Cluster" : "Single",
+    opp: formElements["computer"].checked ? "CPU" : "Player",
+  });
+
+  updateSettings();
+  event.preventDefault();
+  SETTINGS.close();
 }
